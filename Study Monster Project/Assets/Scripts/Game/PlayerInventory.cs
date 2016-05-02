5a57ddc9ec0bd1569c98627e954d2ba555fcd529
HemangRajvanshy <tobiuchiha1010@gmail.com>
2016-05-02 20:30:49 +0530

    Font scaling and smoothened motion Doc Update.

---
+++ Study Monster Project/Assets/Scripts/Game/PlayerInventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+    private Dictionary<Item, int> Items;
+}
+
+[Serializable]
+public class Item
+{
+    public string Name;
+    public Sprite Image;
+}
