59a59073d3e3b19cf3fa153e62e852ffbfcead71
HemangRajvanshy <tobiuchiha1010@gmail.com>
2016-05-21 09:59:37 +0530

    Some sprites for prototyping and some code.

---
+++ Study Monster Project/Assets/Scripts/Game/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DoorScript : MonoBehaviour { //Enables and disables the Room gameobject and also listens to the event.
+
+    public GameObject RoomObject;
+
+    private bool IsIn = false;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            if (IsIn)
+                GoOut();
+            else
+                GoIn();
+        }
+    }
+
+    private void GoIn()
+    {
+        RoomObject.SetActive(true); // Maybe also teleport camera to new location to effectively hide the background.
+        IsIn = true;
+    }
+
+    private void GoOut()
+    {
+        RoomObject.SetActive(false);
+        IsIn = false;
+    }
+
+}
